1c462db826444d66c9865a6fca76c81cf7cb247a
GytisDev <gytis.barakauskas@ktu.edu>
2018-11-05 13:45:51 +0200

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    [SerializeField]
+    float distanceSpeedMultiplier = 0.5f;
+
+    Rigidbody2D rigidboby;
+    LineRenderer lineRenderer;
+
+    bool launched = false;
+
+	// Use this for initialization
+	void Start () {
+
+        rigidboby = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+
+        if (rigidboby)
+            rigidboby.velocity = new Vector2(5f, 5f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TurnLineOn() {
+        lineRenderer.enabled = true;
+    }
+
+    public void TurnLineOff() {
+        lineRenderer.enabled = false;
+    }
+
+    public void DrawLineFromBall(Vector2 pos) {
+
+        
+        Vector3[] positions = new Vector3[2] { transform.position, pos };
+        lineRenderer.SetPositions(positions);
+
+    }
+
+    public void LaunchBall(Vector2 pos) {
+        rigidboby.constraints = RigidbodyConstraints2D.None;
+
+        float distance = Vector2.Distance(transform.position, pos);
+
+
+        rigidboby.velocity = new Vector2(-pos.x * distance * distanceSpeedMultiplier, -pos.y * distance * distanceSpeedMultiplier);
+
+    }
+
+    public void ResetBall() {
+        transform.position = Vector3.zero;
+        rigidboby.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+}
---
+++ Assets/BallTouchControlls.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTouchControlls : MonoBehaviour {
+
+    BallController ballController;
+
+    private bool ballTouched = false;
+
+    private void Start() {
+        ballController = GetComponent<BallController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Debug.Log(ballTouched);
+
+        if (Input.touchCount > 0) {
+     
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+            pos.z = 0;
+
+            Vector2 pos2D = new Vector2(pos.x, pos.y);
+
+            if (touch.phase == TouchPhase.Began) {
+
+                RaycastHit2D hit = Physics2D.Raycast(pos2D, Camera.main.transform.forward);
+
+                if (hit.collider != null) {
+                    switch (hit.collider.gameObject.tag) {
+                        case "Ball":
+                            ballTouched = true;
+                            ballController.TurnLineOn();
+                            break;
+                        default:
+
+                            break;
+                    }
+                }
+
+            } else if (touch.phase == TouchPhase.Ended && ballTouched) {
+                ballTouched = false;
+                ballController.TurnLineOff();
+                //Paleisti
+                ballController.LaunchBall(pos2D);
+            }
+
+            if (ballTouched) {
+                ballController.DrawLineFromBall(pos2D);
+            }
+
+        }
+
+    }
+}
